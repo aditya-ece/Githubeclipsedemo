0b95c7f5104a5fc907578e964bf3a092846987c4
AdityaSanda <adityavardhansanda02@gmail.com>
2022-11-14 15:13:37 +0530

    second commit

--- GitHubEclipsedemo2/src/com/lti/demo/userApp.java
+++ GitHubEclipsedemo2/src/com/lti/demo/userApp.java
@@ -4,5 +4,7 @@ public class userApp {
 	public static void main(String[] args) {
 		Employee e = new Employee(101,"jay",5000);
 		System.out.println(e);
+		System.out.println(e);
+		System.out.println(e);
 	}
 }
